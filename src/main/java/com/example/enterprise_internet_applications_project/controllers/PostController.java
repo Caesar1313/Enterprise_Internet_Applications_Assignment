c545337eaf74141fb227d2cf99861e7a5903154d
Caesar <caesarfarah@yahoo.com>
2022-11-14 22:47:31 +0300

    implement file upload.

--- src/main/java/com/example/enterprise_internet_applications_project/controllers/PostController.java
+++ src/main/java/com/example/enterprise_internet_applications_project/controllers/PostController.java
@@ -1,11 +1,33 @@
 package com.example.enterprise_internet_applications_project.controllers;
 
+import com.example.enterprise_internet_applications_project.models.MyFile;
+import com.example.enterprise_internet_applications_project.repos.upload.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 @RestController
 @RequestMapping
 public class PostController {
 
+    @PostMapping("/uploadFile")
+    public ResponseEntity<MyFile> uploadFile(@RequestParam("file")MultipartFile multipartFile) throws IOException {
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        long size = multipartFile.getSize();
+
+        FileUploadUtil.saveFile(fileName, multipartFile);
+
+        MyFile file = new MyFile();
+        file.setFileName(fileName);
+        file.setSize(size);
+        //file.setDownloadUri("/downloadFile");
+
+        return new ResponseEntity<>(file, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/enterprise_internet_applications_project/models/MyFile.java
+++ src/main/java/com/example/enterprise_internet_applications_project/models/MyFile.java
@@ -1,4 +1,31 @@
 package com.example.enterprise_internet_applications_project.models;
 
 public class MyFile {
+    private String fileName;
+    //private String downloadUri;
+    private long size;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+//    public String getDownloadUri() {
+//        return downloadUri;
+//    }
+//
+//    public void setDownloadUri(String downloadUri) {
+//        this.downloadUri = downloadUri;
+//    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
 }
