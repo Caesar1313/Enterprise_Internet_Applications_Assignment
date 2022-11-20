1b62cfa908acd81a9d0ea5b51688abed030c01d8
Ahmad Abotrab <abotrab.ahmad3099@gmail.com>
2022-11-20 21:14:34 +0300

    process on files

--- src/main/java/com/example/enterprise_internet_applications_project/models/MyFile.java
+++ src/main/java/com/example/enterprise_internet_applications_project/models/MyFile.java
@@ -8,21 +8,38 @@ public class MyFile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    // -- attribute -- //
     private int id;
-
     private String name;
+    private boolean isCheckIn;
 
+    // -- constructor -- //
     public MyFile() {
     }
 
-    public MyFile(String name){
+    public MyFile(String name) {
         this.name = name;
+
     }
 
+    public MyFile(boolean isCheckIn) {
+        this.isCheckIn = isCheckIn;
+    }
+
+    public MyFile(String name, boolean isCheckIn) {
+        this.name = name;
+        this.isCheckIn = isCheckIn;
+    }
+
+    // -- getter and setter -- //
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -30,6 +47,14 @@ public class MyFile {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isIsCheckIn() {
+        return isCheckIn;
+    }
+
+    public void setIsCheckIn(boolean status) {
+        this.isCheckIn = status;
     }
 
 }
--- src/main/java/com/example/enterprise_internet_applications_project/repos/FilesRepository.java
+++ src/main/java/com/example/enterprise_internet_applications_project/repos/FilesRepository.java
@@ -2,9 +2,25 @@ package com.example.enterprise_internet_applications_project.repos;
 
 import com.example.enterprise_internet_applications_project.models.MyFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface FilesRepository extends JpaRepository<MyFile, Integer> {
+
+    @Query(value = "SELECT f FROM MyFile as f WHERE f.name = ?1")
+    MyFile findByName(String nameFile);
+
+    @Query(value = "SELECT f.isCheckIn FROM MyFile as f WHERE f.name = ?1")
+    Optional<Boolean> checkStatusFile(String nameFile);
+
+    @Modifying
+    @Query("DELETE FROM MyFile f WHERE f.name = ?1")
+    ResponseEntity<?> deleteFileByName(String nameFile);
+
 
 }
