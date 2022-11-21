777b35f9046e356fa7d4b3daae80190ef2eb5419
Caesar <caesarfarah@yahoo.com>
2022-11-21 18:23:28 +0300

    aaa

--- src/main/java/com/example/enterprise_internet_applications_project/repositories/FilesRepository.java
+++ src/main/java/com/example/enterprise_internet_applications_project/repositories/FilesRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FilesRepository extends JpaRepository<MyFile, Integer> {
+public interface FilesRepository extends JpaRepository<MyFile, Long> {
 
 }
