b195a38edf28f8f5b07cd29452a7defc14329266
SalemAlashour2 <90504661+SalemAlashour2@users.noreply.github.com>
2022-12-12 11:01:03 +0300

    load balancing

--- src/main/java/com/example/enterprise_internet_applications_project/EnterpriseInternetApplicationsProjectApplication.java
+++ src/main/java/com/example/enterprise_internet_applications_project/EnterpriseInternetApplicationsProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.enterprise_internet_applications_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EnterpriseInternetApplicationsProjectApplication {
 
     public static void main(String[] args) {
